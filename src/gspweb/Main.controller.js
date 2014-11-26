a2f94d6972aaea2b295e01803fff6ed9d5a1fa64
zqy117 <zqy1172003@163.com>
2014-11-26 09:49:09 +0800

    project modify

--- src/gspweb/Main.controller.js
+++ src/gspweb/Main.controller.js
@@ -14,6 +14,7 @@ sap.ui.core.mvc.Controller.extend("inspur.gsp.gspweb.Main", {
 	},
 	//DOM元素加载完成后
 	onAfterRendering:function(){
+		num = 1;
 		var oView = this.getView();
 		var oModelForMenuTree = oView.getModel("oModelForMenuTree");	// 加载右侧菜单数据
 		// var oTransTree = new sap.ui.commons.Tree("treemenu");
@@ -23,29 +24,29 @@ sap.ui.core.mvc.Controller.extend("inspur.gsp.gspweb.Main", {
 		var paneid=this.byId("pi_browser").getId();
 		var that = this;
 		$.ajax({  
-		        	// url: "http://localhost:8080/user/object",		// 取得menu数据
-		        	url: "http://localhost:8080/model",		// 取得menu数据
-		        	type: "Get",
-		        	dataType: "json",
-		        	//sync:false,
-		        	// data: {name:"test"},
-		       		success: function (json) {
-		       			debugger
-		       			//eval('console.log(myshell)')
-		       			// var treemenu=owner.byId("treemenu");
-		       			for (var i = 0; i <json.menuList.length; i++) {
-		       				var childNode = new sap.ui.commons.TreeNode(json.menuList[i].name,{text:json.menuList[i].name});
-		       				// treemenu.addNode(new sap.ui.commons.TreeNode(json.menuList[i].name,{text:json.menuList[i].name}));
-		       				for (var j = 0; j < json.menuList[i].menuList.length; j++) {
-		       					// var lastChild = new sap.ui.commons.TreeNode(json.menuList[i].menuList[j].name,{text:json.menuList[i].menuList[j].name});
-		       					childNode.addNode(new sap.ui.commons.TreeNode(json.menuList[i].menuList[j].name,{text:json.menuList[i].menuList[j].name}));
-		       				};
-		       				treemenu.addNode(childNode);
-		       			};
-		        		oModelForMenuTree.setData(json);
-		        		oModelForMenuTree.refresh();
-	        		}
-	        	}); 
+        	// url: "http://localhost:8080/user/object",		// 取得menu数据
+        	url: "http://localhost:8080/model",		// 取得menu数据
+        	type: "Get",
+        	dataType: "json",
+        	//sync:false,
+        	// data: {name:"test"},
+       		success: function (json) {
+       			// debugger
+       			//eval('console.log(myshell)')
+       			// var treemenu=owner.byId("treemenu");
+       			for (var i = 0; i <json.menuList.length; i++) {
+       				var childNode = new sap.ui.commons.TreeNode(json.menuList[i].name,{text:json.menuList[i].name});
+       				// treemenu.addNode(new sap.ui.commons.TreeNode(json.menuList[i].name,{text:json.menuList[i].name}));
+       				for (var j = 0; j < json.menuList[i].menuList.length; j++) {
+       					// var lastChild = new sap.ui.commons.TreeNode(json.menuList[i].menuList[j].name,{text:json.menuList[i].menuList[j].name});
+       					childNode.addNode(new sap.ui.commons.TreeNode(json.menuList[i].menuList[j].name,{text:json.menuList[i].menuList[j].name}));
+       				};
+       				treemenu.addNode(childNode);
+       			};
+        		oModelForMenuTree.setData(json);
+        		oModelForMenuTree.refresh();
+    		}
+    	})
 		myshell.openPane(paneid);
 		myshell.setPaneContent(treemenu);
 		tabstrip=this.byId("tabstrip");
@@ -89,6 +90,7 @@ sap.ui.core.mvc.Controller.extend("inspur.gsp.gspweb.Main", {
 	},
 	//点击树节点
 	tree_click:function(oControlEvent){
+
 		var oiframe=this.byId("iframecontent");
 		var oifra=this.byId("iframecont");
 		var currentnode = oControlEvent.getParameter("node");
@@ -100,7 +102,11 @@ sap.ui.core.mvc.Controller.extend("inspur.gsp.gspweb.Main", {
         if (selectedNode != undefined) {
         	switch (nodetext) {
 				case "Vegetarian diet":
-					var oTab1 = new sap.ui.commons.Tab({title: new sap.ui.core.Title({text:nodetext}), closable: true, content: [oiframe]});
+					var oTab1 = new sap.ui.commons.Tab({
+						title: new sap.ui.core.Title({text:nodetext})
+						, closable: true
+						, content: [oiframe]
+					});
 					tabstrip.addTab(oTab1);
 					break;
 				case "Vegetables":
@@ -108,7 +114,16 @@ sap.ui.core.mvc.Controller.extend("inspur.gsp.gspweb.Main", {
 					tabstrip.addTab(oTab2);
 					break;
 				default:
-					var oTab = new sap.ui.commons.Tab({title: new sap.ui.core.Title({text:nodetext}), closable: true, content: [TextView]});
+					// 创建iframe
+					// var iframe=new sap.ui.core.HTML({
+					// 	content:"<iframe src='http://localhost:9000/index2.html' width='100%' height='100%' scrolling='no' framespacing='0' border='0' frameborder='0'></ifram>"});
+					var icomp = new sap.ui.core.ComponentContainer({
+						name: "inspur.gsp.form"
+					});
+					var oTab = new sap.ui.commons.Tab({
+						title: new sap.ui.core.Title({text:nodetext}), 
+						closable: true, 
+						content: [icomp]});
 					tabstrip.addTab(oTab);
 					break;
 				}        	
