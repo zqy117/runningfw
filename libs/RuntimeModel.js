0e56340c7e5b5f2a745e902a04d4b0e683991278
仪 思奇 <Will@yisq.gsp>
2014-12-01 09:14:52 +0800

    add RuntimeComponent and RuntimeModel

---
+++ libs/RuntimeModel.js
@@ -0,0 +1,18 @@
+jQuery.sap.declare("inspur.gsp.commons.RuntimeModel")
+
+sap.ui.model.json.JSONModel.extend("inspur.gsp.commons.RuntimeModel", {
+
+  constructor : function(arg) {
+    if (arg.constructor == sap.ui.commons.TabStrip) {
+      this.container = arg
+      sap.ui.model.json.JSONModel.apply(this)
+    } else {
+      sap.ui.model.json.JSONModel.apply(this, arguments)
+    }
+  }
+
+  , openFunc  : function(sComponentId, oCallbacks) {
+    debugger
+  }
+  
+})
