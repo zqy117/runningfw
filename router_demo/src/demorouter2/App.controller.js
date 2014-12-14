5583a0a36a82fbde93e94d82775bbc2c517a312c
zqy117 <zqy1172003@163.com>
2014-12-15 06:06:54 +0800

    delete index files

--- router_demo/src/demorouter2/App.controller.js
+++
@@ -1,9 +0,0 @@
-
-jQuery.sap.declare("inspur.gsp.App")
-
-sap.ui.core.mvc.Controller.extend("inspur.gsp.App", {
-	onTest : function(){
-		alert(sap.ui.getCore().byId("__xmlview0--appContainer"));
-		alert(this.byId("appContainer"));
-	}
-})
